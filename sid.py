1c5e39273007378d8c5927e0209aae4083475bac
adity72dd <mmunnabot7@gmail.com>
2025-02-04 14:28:57 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -9,7 +9,7 @@ from config import BOT_TOKEN, OWNER_USERNAME, CHANNEL_LINK, CHANNEL_LOGO
 USER_FILE = "users.json"
 ADMIN_FILE = "admins.json"
 DEFAULT_THREADS = 1500
-DEFAULT_PACKET = 9
+DEFAULT_PACKET = 8
 DEFAULT_DURATION = 200  # Default attack duration
 
 users = {}
