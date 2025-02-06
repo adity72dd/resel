29920623c0cb6270c13fef63b12eb4da72d3bce6
adity72dd <mmunnabot7@gmail.com>
2025-02-06 20:09:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ OWNER_USERNAME = "Riyahacksyt"
 ADMIN_IDS = ["6479495033"]  # Keep it as a list for multiple admins
 
 # Enter your Telegram channel link
-CHANNEL_LINK = "https://t.me/+EiUj5ibb1V1kZDM1"  # Replace with your actual channel link
+CHANNEL_LINK = "https://t.me/+LKefI2JDQqAzMmQ1"  # Replace with your actual channel link
 
 # Enter the filename of your channel logo (Image must be in the same folder as the script)
 CHANNEL_LOGO = "channel_logo.png"  # Replace with actual image file name
