7744d24715e3278e5f533e9890484041abdfa953
adity72dd <mmunnabot7@gmail.com>
2025-02-06 20:18:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ OWNER_USERNAME = "Riyahacksyt"
 # Enter your admin ID (Get it from @MissRose_bot by typing /info)
 ADMIN_IDS = ["6479495033"]  # Keep it as a list for multiple admins
 
-# Enter your Telegram channel link
+# Enter your Telegram channel link (make sure the link doesn't contain characters that break Markdown)
 CHANNEL_LINK = "https://t.me/+LKefI2JDQqAzMmQ1"  # Replace with your actual channel link
 
 # Enter the filename of your channel logo (Image must be in the same folder as the script)
