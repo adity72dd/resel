2f4e9f5b562ebcea8bcc578a148dcb5b40518ce0
adity72dd <mmunnabot7@gmail.com>
2025-02-08 23:07:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,19 @@
-# SCRIPT BY: Riyahacksyt  
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7826805459:AAFv42oAhypOTC02e6kWk1Astp_1PcW3h0Y")
 
-# Enter your Bot Token (Get it from @BotFather)
-BOT_TOKEN = "7826805459:AAFv42oAhypOTC02e6kWk1Astp_1PcW3h0Y"
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Riyahacksyt")
 
-# Enter your Telegram username (without @)
-OWNER_USERNAME = "Riyahacksyt"
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6479495033")
 
-# Enter your admin ID (Get it from @MissRose_bot by typing /info)
-ADMIN_IDS = ["6479495033"]  # Keep it as a list for multiple admins
 
-# Enter your Telegram channel link (make sure the link doesn't contain characters that break Markdown)
-CHANNEL_LINK = "https://t.me/+LKefI2JDQqAzMmQ1"  # Replace with your actual channel link
 
-# Enter the filename of your channel logo (Image must be in the same folder as the script)
-CHANNEL_LOGO = "channel_logo.png"  # Replace with actual image file name
+
+
+
+
+  
