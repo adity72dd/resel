f2d1406514e603585ec1b696468327605678dd57
adity72dd <mmunnabot7@gmail.com>
2025-02-11 19:51:53 +0530

    Update sid.py

--- sid.py
+++ sid.py
@@ -8,9 +8,9 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
 
 USER_FILE = "users.json"
-DEFAULT_THREADS = 1600
+DEFAULT_THREADS = 300
 DEFAULT_PACKET = 10
-DEFAULT_DURATION = 150  # Default attack duration
+DEFAULT_DURATION = 180  # Default attack duration
 BAN_DURATION = 1000  # Ban duration (10 mi22nutes)
 
 users = {}
